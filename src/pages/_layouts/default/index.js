ba8e3827c9e1d4517fcdcf2d406e275130b09044
Mônica Santos <monicasantosdev@gmail.com>
2020-02-25 17:46:45 -0300

    create header component

--- src/pages/_layouts/default/index.js
+++ src/pages/_layouts/default/index.js
@@ -2,9 +2,15 @@ import React from 'react'
 import { element } from 'prop-types'
 
 import { Wrapper } from './styles'
+import { Header } from '~/components/Header'
 
 const DefaultLayout = ({ children }) => {
-  return <Wrapper>{children}</Wrapper>
+  return (
+    <Wrapper>
+      <Header />
+      {children}
+    </Wrapper>
+  )
 }
 
 DefaultLayout.propTypes = {
